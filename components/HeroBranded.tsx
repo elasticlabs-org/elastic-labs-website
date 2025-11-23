2f2bf1257441034e131a83d56ff91532049ac7b4
chungquantin <tin@onrover.com>
2025-11-23 08:40:56 +0700

    chore: remove scrollbar indicator

--- components/HeroBranded.tsx
+++ components/HeroBranded.tsx
@@ -156,22 +156,6 @@ export function HeroBranded() {
           </motion.div>
         </div>
       </div>
-
-      {/* Scroll Indicator */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1.5, duration: 1 }}
-        className="absolute bottom-8 left-1/2 transform -translate-x-1/2 z-30"
-      >
-        <motion.div
-          animate={{ y: [0, 10, 0] }}
-          transition={{ duration: 1.5, repeat: Infinity }}
-          className="w-6 h-10 border-2 border-white/40 rounded-full flex justify-center"
-        >
-          <div className="w-1 h-2 bg-white/60 rounded-full mt-2"></div>
-        </motion.div>
-      </motion.div>
     </section>
   );
 }
